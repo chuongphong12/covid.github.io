c99d9023caef4f8b0c9542dc25e4eb349bd98845
Trannhan <chuongphong12@gmail.com>
2020-05-20 12:04:33 +0700

    Updates

---
+++ precache-manifest.d4e1c5d996d5f70d648cbdd94276b8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9baab1e641bb592b1a8dce0d995cf5e3",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "143533502c21c4d1503d",
+    "url": "/covid-19/static/css/main.9e29e58a.chunk.css"
+  },
+  {
+    "revision": "55358e937382e04c3c6a",
+    "url": "/covid-19/static/js/2.00509692.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.00509692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143533502c21c4d1503d",
+    "url": "/covid-19/static/js/main.2748db70.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
